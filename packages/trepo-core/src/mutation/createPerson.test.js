3ef242cd9643a03dba91c5e8f526eb292653ab4b
John Clark <socrates37@gmail.com>
2016-10-21 09:53:54 -0600

    trepo-core - allowed name on createPerson; closes #52 (#55)

--- packages/trepo-core/src/mutation/createPerson.test.js
+++ packages/trepo-core/src/mutation/createPerson.test.js
@@ -14,17 +14,23 @@ describe('commit', () => {
       query: `mutation ($input: PersonCreateInput){
         node: createPerson(input: $input) {
           id
+          name {
+            name
+          }
         }
       }`,
       variables: {
         input: {
+          name: 'my name',
         },
       },
     });
     expect(response).to.have.all.keys('data');
     expect(response.data).to.have.all.keys('node');
     const data = response.data.node;
-    expect(data).to.have.all.keys('id');
+    expect(data).to.have.all.keys('id', 'name');
     expect(data.id).to.not.equal(null);
+    expect(data.name).to.not.equal(null);
+    expect(data.name.name).to.equal('my name');
   });
 });
