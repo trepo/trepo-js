80a8d66e3029b768d2bf00982c10df9e96a46769
John Clark <socrates37@gmail.com>
2016-10-21 08:49:35 -0600

    trepo-ptree - added person delete; closes #17

---
+++ packages/trepo-ptree/src/person/delete.test.js
@@ -0,0 +1,46 @@
+const {expect} = require('chai');
+const func = require('./delete.js');
+const {VGraph} = require('trepo-vgraph');
+const Label = require('../label.js');
+let vGraph;
+
+describe('person - delete', () => {
+  beforeEach(async () => {
+    vGraph = new VGraph('repo');
+    await vGraph.init();
+  });
+
+  it('should cleanup everything', async () => {
+    const node = await vGraph.addNode(Label.PERSON);
+    const id = await node.getId();
+    const nameNode = await vGraph.addNode(Label.NAME);
+    const name = await nameNode.getId();
+    await vGraph.addEdge(Label.NAME_PERSON, node, nameNode);
+    const deathNode = await vGraph.addNode(Label.DEATH);
+    await vGraph.addEdge(Label.DEATH_PERSON, node, deathNode);
+    const death = await deathNode.getId();
+
+    await func({vGraph, input: {id}});
+
+    try {
+      await vGraph.getNode(name);
+      throw new Error('should have errored');
+    } catch (error) {
+      expect(error.message).to.equal('Deleted');
+    }
+
+    try {
+      await vGraph.getNode(death);
+      throw new Error('should have errored');
+    } catch (error) {
+      expect(error.message).to.equal('Deleted');
+    }
+
+    try {
+      await vGraph.getNode(id);
+      throw new Error('should have errored');
+    } catch (error) {
+      expect(error.message).to.equal('Deleted');
+    }
+  });
+});
